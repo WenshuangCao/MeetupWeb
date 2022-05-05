b9492f3e7123287d5935cae415675a1b5bcfa3c5
Wenshuang <ainsleycao@gmail.com>
2022-05-05 16:28:43 +1000

    add api

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+// /api/new-meetup
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://user-1:user-111111@cluster0.qvkc4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
